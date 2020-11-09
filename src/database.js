98fbdbfc8d9c264c83ffaf0739612b1acb150b95
alexfrenkel92 <alex.frenkel92@gmail.com>
2020-11-10 00:31:43 +0100

    database connection solved

--- src/database.js
+++ src/database.js
@@ -3,6 +3,10 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 
 const conn = mysql.createConnection({
+    // host: 'chargeport.c7ee1zvs2c38.us-east-2.rds.amazonaws.com',
+    // user: 'chargeport',
+    // password: 'chargeport01',
+    // database: 'chargeport'
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
